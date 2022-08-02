438e82249cd76ebaf09151e6ebf1b141c25d8aa2
LouiLetsPlayPro <51512114+LouiLetsPlayPro@users.noreply.github.com>
2022-08-02 20:17:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { Console } = require('console');
+const { Transform } = require('stream');
+
 function getTimestamp() {
     var timeNow = new Date();
     var timestamp = "" + timeNow.toLocaleTimeString() + " " + timeNow.toLocaleDateString()
@@ -52,6 +55,7 @@ function logOperation(message, logchannel, client, status, type) {
 module.exports.console = class {
 
     /**
+     * Richte das Consolen System ein
      * @param {boolean} channellog Gebe an, ob das Consolen modul über einen Discord Bot mit loggen soll
      * @param {discord.CategoryChannel.id} logchannel Gebe die Channel ID deines Discord Log Channel an
      * @param {boolean} time Gebe an, ob eine Zeitangabe mit geloggt werden soll
@@ -60,6 +64,8 @@ module.exports.console = class {
      * 
      * @example const { console } = require('./modules/console')
      *          console.consolebuilder(false, 0, true, true, client)
+     * 
+     * @since v1.0.0
      */
     static consolebuilder(
         channellog = false,
@@ -80,6 +86,8 @@ module.exports.console = class {
      * @param {string} message Nachricht
      * 
      * @example console.log("Moin")
+     * 
+     * @since v1.0.0
     */
     static log(message) {
         if (this.channellog == true) {
@@ -104,6 +112,8 @@ module.exports.console = class {
      * @param {string} message Nachricht dazu
      * 
      * @exaple console.logError(error, "Alles Ok")
+     * 
+     * @since v1.0.0
      */
     static logError(code, message) {
         if (this.channellog == true) {
@@ -127,6 +137,8 @@ module.exports.console = class {
      * @param {string} message Warnung
      * 
      * @example console.logWarning("Moin")
+     * 
+     * @since v1.0.0
     */
     static logWarning(message) {
         if (this.channellog == true) {
@@ -152,6 +164,8 @@ module.exports.console = class {
      * @param {string} message Operationsnachricht
      * 
      * @example console.logOperation("Login","COMLPLEATE","erfolgreich")
+     * 
+     * @since v1.0.0
      */
     static logOperation(type, status, message) {
         if (this.channellog == true) {
@@ -176,6 +190,8 @@ module.exports.console = class {
      * @param {string} message Nachricht zum Status
      * 
      * @example console.logStatus("Online", "System erfolgreich gestartet")
+     * 
+     * @since v1.0.0
      */
     static logStatus(status, message) {
         if (this.channellog == true) {
@@ -192,7 +208,122 @@ module.exports.console = class {
         }
         if (this.time == false && this.color == false) {
             console.log("[ " + status + " ]: " + message);
-
         }
     };
+
+    /**
+     * Leere die Console
+     * 
+     * @example console.logclear()
+     * 
+     * @since v2.0.0
+     */
+    static logClear() {
+        if (this.channellog == true) {
+            log("console cleared", this.logchannel, this.client)
+        }
+        console.clear()
+    }
+
+    /**
+     * Stelle daten in der Console in einer Tabelle dar
+     * @param {Array} input Array der Daten die in der Tabelle angezeigt werden sollen
+     * @param {Array} properties Welche Argumente sollen angezeigt werden - Weck lassen oder leres Array lässt alles anzeigen - Index ausgeschlossen!
+     * @param {boolean} index Wenn true wird die Spalte (index) nicht angezeigt
+     * 
+     * @example console.logTable([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }], [], false)
+     * @example console.logTable([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }], ["a"], false)
+     * @example console.logTable([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }], ["a"], true)
+     * 
+     * @since v2.0.0
+     */
+    static logTable(input, properties, index) {
+        if(!properties || !properties[0]){
+            properties = []
+            for (let i = 0; i < input.length; i++) {
+                for (const key in input[i]) {
+                    properties.push(key)
+                }
+            }
+        }
+        if(!index){
+            index = false
+        }
+        const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
+        const logger = new Console({ stdout: ts })
+        logger.table(input, properties)
+        const table = (ts.read() || '').toString()
+        let result = '';
+        if (this.channellog == true) {
+            for (let row of table.split(/[\r\n]+/)) {
+                let r = row
+                if(index == false){
+                    r = r.replace(/[^┬]*┬/, '┌');
+                    r = r.replace(/^├─*┼/, '├');
+                    r = r.replace(/│[^│]*/, '');
+                    r = r.replace(/^└─*┴/, '└');
+                    r = r.replace(/'/g, ' ');
+                }
+                result += `${getTimestamp()} ${r}\n`;
+            }
+            logTable(result, this.logchannel, this.client)
+        }
+
+        if (this.time == true && this.color == true) {
+            for (let row of table.split(/[\r\n]+/)) {
+                let r = row
+                if(index == false){
+                    r = r.replace(/[^┬]*┬/, '┌');
+                    r = r.replace(/^├─*┼/, '├');
+                    r = r.replace(/│[^│]*/, '');
+                    r = r.replace(/^└─*┴/, '└');
+                    r = r.replace(/'/g, ' ');
+                }
+                result += `\u001b[30;1m${getTimestamp()} \u001b[32m ${r}\n`;
+            }
+            console.log(result + "\u001b[0m");
+        }
+        if (this.time == false && this.color == true) {
+            for (let row of table.split(/[\r\n]+/)) {
+                let r = row
+                if(index == false){
+                    r = r.replace(/[^┬]*┬/, '┌');
+                    r = r.replace(/^├─*┼/, '├');
+                    r = r.replace(/│[^│]*/, '');
+                    r = r.replace(/^└─*┴/, '└');
+                    r = r.replace(/'/g, ' ');
+                }
+                result += `\u001b[32m ${r}\n`;
+            }
+            console.log(result + "\u001b[0m");
+        }
+        if (this.time == true && this.color == false) {
+            for (let row of table.split(/[\r\n]+/)) {
+                let r = row
+                if(index == false){
+                    r = r.replace(/[^┬]*┬/, '┌');
+                    r = r.replace(/^├─*┼/, '├');
+                    r = r.replace(/│[^│]*/, '');
+                    r = r.replace(/^└─*┴/, '└');
+                    r = r.replace(/'/g, ' ');
+                }
+                result += `${getTimestamp()} ${r}\n`;
+            }
+            console.log(result);
+        }
+        if (this.time == false && this.color == false) {
+            for (let row of table.split(/[\r\n]+/)) {
+                let r = row
+                if(index == false){
+                    r = r.replace(/[^┬]*┬/, '┌');
+                    r = r.replace(/^├─*┼/, '├');
+                    r = r.replace(/│[^│]*/, '');
+                    r = r.replace(/^└─*┴/, '└');
+                    r = r.replace(/'/g, ' ');
+                }
+                result += `${r}\n`;
+            }
+            console.log(result);
+        }
+    }
 };
